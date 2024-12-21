1a17aa970eaf7d16f3cc420f04980bdc1a4d1af0
vishalsgite <vshlsgite.98.vg@gmail.com>
2024-12-21 17:58:04 +0530

    Primitive Data Type In JAVA

--- src/Day_01/ThirdProgram.java
+++ src/Day_01/ThirdProgram.java
@@ -1,10 +1,37 @@
 package Day_01;
 
-
+//Primitive Data Type In JAVA
 
 public class ThirdProgram {
 
     public static void main(String[] args) {
+
+        int rollNo = 47;
+        char ch = 'r';
+        float marks = 98.54f;
+        double largeDecimalNumbers = 4944551.4984848;
+        long largeIntergerValues = 889518878514L;
+        boolean check = true;
+
+        /*
+        First of All , String is not a primitive data type . we will learn what is primitive data type on String
+        Data type ,
+
+           Primitiveis is basically , any data type we cannot break even further .
+           for ex . String = "Vishal" -- > here , we can break this string into chars like , V , i , s , h , a ,l
+           so String is not a primitive data type .
+
+         Primitive Data Types --> the last data type , which we cannot break into other data type.
+                                 we can say ,it is primitive data type.
+
+                                 Int ,char , float , double , long , boolean
+
+
+      why we are adding f or L here --> float marks = 98.54f & long largeIntergerValues = 889518878514L;
+
+      --ans : basically , all decimal values by default they are type of double . if we want to store in float
+      then we need to add 'f' over there and same for long also .
+         */
 
     }
 }
